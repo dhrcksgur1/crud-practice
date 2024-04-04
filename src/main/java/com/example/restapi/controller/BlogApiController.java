ef7e64a4595ab84484b3a5f2f1282de062003508
ChanHyeok <dhrcksgur1@gmail.com>
2024-04-04 13:07:18 +0900

    아티클 목록 조회

--- src/main/java/com/example/restapi/controller/BlogApiController.java
+++ src/main/java/com/example/restapi/controller/BlogApiController.java
@@ -2,14 +2,18 @@ package com.example.restapi.controller;
 
 import com.example.restapi.domain.Article;
 import com.example.restapi.dto.AddArticleRequest;
+import com.example.restapi.dto.ArticleResponse;
 import com.example.restapi.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -25,4 +29,15 @@ public class BlogApiController {
                 .status(HttpStatus.CREATED)
                 .body(savedArticle);
     }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok().body(articles);
+    }
+
 }
--- src/main/java/com/example/restapi/service/BlogService.java
+++ src/main/java/com/example/restapi/service/BlogService.java
@@ -6,6 +6,8 @@ import com.example.restapi.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor //생성자
 public class BlogService {
@@ -15,4 +17,9 @@ public class BlogService {
     public Article save(AddArticleRequest request){
         return blogRepository.save(request.toEntity());
     }
+
+    public List<Article> findAll(){
+        return blogRepository.findAll();
+    }
+
 }
